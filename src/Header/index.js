ce8a4a93dd410db10fedaffa35faf4d45b5bde73
crimemp <crimsnemptnss@yandex.ru>
2020-07-14 23:52:19 +0500

    add header

---
+++ src/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css'
+
+function Header() {
+	return (
+		<header>
+			<img src='images/logo.png' alt="logo"></img>
+			<h3>STUDENTS</h3>
+		</header>
+	);
+	
+}
+
+export default Header
